ba0fc326a0627b2a70669f3431b47723eda9df7d
Jswig <andersjopo@gmail.com>
2019-12-03 12:01:25 -0800

    increased embeddings size and regularization

--- predicting_ratings/models/lstm.py
+++ predicting_ratings/models/lstm.py
@@ -4,12 +4,12 @@
 # 13-11-2019
 
 import tensorflow as tf
-from tensorflow.keras.layers import Dropout, Dense, Embedding, LSTM,
-                                    Bidirectional
+from tensorflow.keras.layers import Dropout, Dense, Embedding, LSTM, Bidirectional
+from tensorflow.keras import regularizers
 
 def lstm_model(optimizer = 'adam', loss = 'categorical_crossentropy', 
                metrics = ['accuracy'], num_units = 128,
-               embedding_size = 16):
+               embedding_size = 16, dropout = 0.2):
     """
 
     Returns: (keras Model) compiled keras model
@@ -20,7 +20,12 @@ def lstm_model(optimizer = 'adam', loss = 'categorical_crossentropy',
     model = tf.keras.Sequential()
     model.add(Embedding(input_dim = 10000, output_dim = embedding_size,
                         input_length = 1050))
-    model.add(Bidirectional(LSTM(num_units)))
+    model.add(Bidirectional(LSTM(num_units,
+      dropout = dropout, recurrent_dropout = dropout,
+      # kernel_regularizer = regularizers.l1(0.01),
+      recurrent_regulizer = regularizers.l1(0.01),
+      # activity_regularizer = regularizers.l1(0.01),
+      bias_regularizer = regularizers.l1(0.01))))
     model.add(Dense(5, activation = 'softmax'))
     model.compile(optimizer = optimizer, loss = loss, metrics = metrics)
     
